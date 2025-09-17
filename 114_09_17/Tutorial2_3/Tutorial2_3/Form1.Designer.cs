e0e75ef5005c4688799991215f344d13899e1aec
ding-yii <a112221061@mail.shu.edu.tw>
2025-09-17 13:03:44 +0800

    114-09-17

---
+++ 114_09_17/Tutorial2_3/Tutorial2_3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Tutorial2_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("華康新特圓體", 20F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label1.Location = new Point(148, 45);
+            label1.Name = "label1";
+            label1.Size = new Size(477, 40);
+            label1.TabIndex = 0;
+            label1.Text = "選擇一個語言,我會說早安";
+            // 
+            // label2
+            // 
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.Font = new Font("Microsoft JhengHei UI", 14F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            label2.Location = new Point(100, 152);
+            label2.Name = "label2";
+            label2.Size = new Size(570, 95);
+            label2.TabIndex = 1;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 23F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "語言翻譯器";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+    }
+}
